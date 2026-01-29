9b01c8ee9d24ded0d351ff72d0a894e35c44a3a6
ap73mka <artyom.vlasov.05@mail.ru>
2026-01-29 16:59:23 +0300

    msw in production

--- plugins/msw.client.ts
+++ plugins/msw.client.ts
@@ -1,8 +1,10 @@
 import { defineNuxtPlugin } from "#imports"
 
 export default defineNuxtPlugin(async () => {
-  if (process.env.NODE_ENV === "development") {
-    const { worker } = await import("../msw/browser")
-    await worker.start({ onUnhandledRequest: "bypass" })
-  }
+  // if (process.env.NODE_ENV === "development") {
+  //   const { worker } = await import("../msw/browser")
+  //   await worker.start({ onUnhandledRequest: "bypass" })
+  // }
+  const { worker } = await import("../msw/browser")
+  await worker.start({ onUnhandledRequest: "bypass" })
 })
--- plugins/msw.server.ts
+++ plugins/msw.server.ts
@@ -1,9 +1,12 @@
 import { defineNuxtPlugin } from "#imports"
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (process.env.NODE_ENV === "development") {
-    const { server } = require("../msw/server")
-    server.listen({ onUnhandledRequest: "bypass" })
-    nuxtApp.hook("app:unmount", () => server.close())
-  }
+  // if (process.env.NODE_ENV === "development") {
+  //   const { server } = require("../msw/server")
+  //   server.listen({ onUnhandledRequest: "bypass" })
+  //   nuxtApp.hook("app:unmount", () => server.close())
+  // }
+  const { server } = require("../msw/server")
+  server.listen({ onUnhandledRequest: "bypass" })
+  nuxtApp.hook("app:unmount", () => server.close())
 })
